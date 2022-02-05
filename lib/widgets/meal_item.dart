7ce2ef3642672a15dc31e83a1d097d02b6f66287
Shuvo hassan <shuvo0201@gmail.com>
2022-02-05 21:41:39 +0600

    Add remove feature

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -10,16 +10,18 @@ class MealItem extends StatelessWidget {
   final int duration;
   final Complexity complexity;
   final Affordability affordability;
+  final Function removeItem;
 
-  const MealItem({
-    Key? key,
-    required this.id,
-    required this.title,
-    required this.imageUrl,
-    required this.duration,
-    required this.complexity,
-    required this.affordability,
-  }) : super(key: key);
+  const MealItem(
+      {Key? key,
+      required this.id,
+      required this.title,
+      required this.imageUrl,
+      required this.duration,
+      required this.complexity,
+      required this.affordability,
+      required this.removeItem})
+      : super(key: key);
 
   String get complexityText {
     switch (complexity) {
@@ -51,10 +53,12 @@ class MealItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
-        Navigator.of(context).pushNamed(
-          MealDetailScreen.routeName,
-          arguments: id,
-        );
+        Navigator.of(context)
+            .pushNamed(
+              MealDetailScreen.routeName,
+              arguments: id,
+            )
+            .then((value) => removeItem(value));
       },
       child: Card(
         shape: const RoundedRectangleBorder(
